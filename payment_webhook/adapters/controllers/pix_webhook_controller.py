482cab5f930690f2647dfea15f8e8976fdd3c079
MrAbade <vini.abade@outlook.com.br>
2024-05-03 11:52:36 -0300

    Chore: Add webhook preffix for pix webhook controller
    dev

--- payment_webhook/adapters/controllers/pix_webhook_controller.py
+++ payment_webhook/adapters/controllers/pix_webhook_controller.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, status
 from fastapi.responses import JSONResponse
 
-pix_webhook_controller = APIRouter()
+pix_webhook_controller = APIRouter(prefix="/webhook")
 
 
 @pix_webhook_controller.post("/", status_code=status.HTTP_201_CREATED)
